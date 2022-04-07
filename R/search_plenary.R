c240e723c60b9912c698edd5a956c48a07894b7c
FrederikHeylen <frederik.heylen@datamarinier.be>
2022-04-07 14:47:23 +0200

    fix select

--- R/search_plenary.R
+++ R/search_plenary.R
@@ -80,15 +80,15 @@ search_plenary <- function(date_range_from,date_range_to,use_parallel=FALSE){
     tidyr::unnest(cols = c(contacttype, id, link, nummer, objectstatus, objecttype, onderwerp,
                     titel, verslagnognietbeschikbaar, zittingsjaar, document,
                     `namens-commissie`),names_sep="_") %>%
-    dplyr::select(id_plenaire_sessie
-           ,datumbegin
-           ,datumeinde
-           ,journaallijn_id
-           ,type_activiteit
-           ,item_id = id
-           ,objecttype_naam
-           ,onderwerp
-           ,titel) %>%
+  dplyr::select(id_plenaire_sessie
+                  ,datumbegin
+                  ,datumeinde
+                  ,journaallijn_id
+                  ,type_activiteit
+                  ,item_id = id_id
+                  ,objecttype_naam
+                  ,onderwerp =onderwerp_onderwerp
+                  ,titel = titel_titel ) %>%
     filter(!is.na(item_id)) -> result
 
   result %>%
